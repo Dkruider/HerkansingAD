0b9e39f56747ae606acea7ca42d186527e08e7cc
Dkruider <David.kruider@hotmail.com>
2019-12-05 22:16:00 +0100

    Add project files.

---
+++ HerkansingAD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HerkansingAD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
