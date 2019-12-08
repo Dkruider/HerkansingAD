d85e6827f091a9211f297ff468412b5d0ef75aae
Dkruider <David.kruider@hotmail.com>
2019-12-08 01:20:52 +0100

    Added ArrayList implementation

---
+++ HerkansingAD/ArrayList/MyArrayList.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Huiswerk2
+{
+    public class MyArrayList : IMyArrayList
+    {
+        private int[] data;
+        private int size;
+
+        public MyArrayList(int capacity)
+        {
+            data = new int[capacity];
+            size = 0;
+        }
+
+        public void Add(int n)
+        {
+            if (size == data.Length) throw new MyArrayListFullException();
+
+            data[size] = n;
+            size++;
+        }
+
+        public int Get(int index)
+        {
+            if (index < 0 || index >= size || size == 0) throw new MyArrayListIndexOutOfRangeException();
+
+            return data[index];
+        }
+
+        public void Set(int index, int n)
+        {
+            if (index < 0 || index >= size || size == 0) throw new MyArrayListIndexOutOfRangeException();
+
+            data[index] = n;
+        }
+
+        public int Capacity()
+        {
+            return data.Length;
+        }
+
+        public int Size()
+        {
+            return size;
+        }
+
+        public void Clear()
+        {
+            size = 0;
+        }
+
+        public int CountOccurences(int n)
+        {
+            int occurrences = 0;
+            for (int i = 0; i < size; i++)
+            {
+                if (data[i].Equals(n)) occurrences++;
+            }
+
+            return occurrences;
+        }
+
+        public bool CheckValueExist(int n)
+        {
+            bool exist = false;
+
+            for (int i = 0; i < size; i++)
+            {
+                if (!data[i].Equals(n)) continue;
+                exist = true;
+                break;
+            }
+
+            return exist;
+        }
+
+        public override string ToString()
+        {
+            int[] temp = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                temp[i] = data[i];
+            }
+
+            return size == 0 ? "NIL" : "[" + string.Join(",", temp) + "]";
+        }
+    }
+}
--- HerkansingAD/Program.cs
+++ HerkansingAD/Program.cs
@@ -1,10 +1,49 @@
 ﻿using System;
+using Huiswerk2;
 using Huiswerk5;
 
 namespace HerkansingAD
 {
     class Program
     {
+
+        static void ArrayList()
+        {
+            System.Console.WriteLine("\n=====   Opgave 1 : MyArrayList   =====\n");
+
+            MyArrayList al = new MyArrayList(10);
+            System.Console.WriteLine(al);
+            al.Add(2);
+            al.Add(3);
+            al.Add(5);
+            System.Console.WriteLine(al);
+            Console.WriteLine(al.Get(0));
+            try
+            {
+                Console.WriteLine(al.Get(3));
+            }
+            catch (MyArrayListIndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            al.Set(2, 4);
+            System.Console.WriteLine(al);
+
+            al.Clear();
+            for (int i = 0; i < 10; i++)
+            {
+                al.Add(i);
+            }
+            al.Set(9, 2);
+            al.Set(7, 2);
+            System.Console.WriteLine(al);
+            Console.WriteLine(al.CountOccurences(2));
+
+            Console.WriteLine($"value exist: {al.CheckValueExist(10)}");
+            Console.WriteLine($"value exist: {al.CheckValueExist(2)}");
+        }
+
+
         static void BinarySearchTree()
         {
             BinarySearchTree<int> bst;
@@ -44,6 +83,7 @@ namespace HerkansingAD
 
         static void Main(string[] args)
         {
+            ArrayList();
             BinarySearchTree();
             PriorityQueue();
 
